b4782140d79cddaa5df48c97e89d2025e821c5aa
Atharva Nandanwar <atharvanandawar@outlook.com>
2018-10-16 19:05:54 +0530

    Shortened the comments and some aesthetic changes

--- python/leap/leap.py
+++ python/leap/leap.py
@@ -1,15 +1,15 @@
-#Program to determine if the given year is leap year or not.
+# Program to determine if the given year is leap year or not.
 def is_leap_year(year):
-    if(year%1 == 0):                            #First conditional to eliminate error conditions
-        if(year%4 == 0):                        #Second conditional to factor in years divisible by 4
-            if(year%100 == 0):                  #Third conditional to factor in years divisible by 100
-                if(year%400 == 0):              #Fourth conditional to factor in years divisible by 400
-                    return(True)                #years divisible by 400 are leap years
+    if(year % 1 == 0):                          # Eliminate error conditions
+        if(year % 4 == 0):                      # Numbers divisible by 4
+            if(year % 100 == 0):                # Numbers divisible by 100
+                if(year % 400 == 0):            # Numbers divisible by 400
+                    return(True)                # Leap year inner condition
                 else:
-                    return(False)               #years not divisible by 400 but divisible by 100 are not leap
+                    return(False)               # Not leap year inner condition
             else:
-                return(True)                    #years normally divisible by 4 but not divisible by 100 are leap
+                return(True)                    # Leap year outer condition
         else:
-            return(False)                       #years not divisible by 4 are not leap
+            return(False)                       # Not leap year outer condition
     else:
-        raise Exception("Not a whole number")   #exception for when input is not a number or integer number
+        raise Exception("Not a whole number")   # exception bad input
